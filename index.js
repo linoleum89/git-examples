8bb023d8bf0fb4de30a4e5d111bfcd0793fc5cb6
Jorge Cortés <jorge@ski.com>
2018-11-20 12:47:11 -0700

    rebase first commit

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ function myGitExample(message) {
     console.log(message);
 }
 
-myGitExample('this is for the merge strategy demo');
+myGitExample('this is for the rebase strategy demo');
 
 function sum(number1, number2) {
     return number1 + number2;
